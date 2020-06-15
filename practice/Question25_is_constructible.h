ee12c108c0b8cd421ba824a32f4c5d713f23c42b
Alexey Kudrinsky <kudrinsky.am@gmail.com>
2020-06-15 14:30:49 +0500

    ready

--- practice/Question25_is_constructible.h
+++ practice/Question25_is_constructible.h
@@ -1,8 +1,32 @@
+#include <iostream>
+#include <type_traits>
 
-template <class T, T value>
-class <#class name#> {
-    <#instance variables#>
+struct S {
+    S (int) {
+        
+    }
     
-public:
-    <#member functions#>
+    S () {
+        
+    }
 };
+
+template <typename T, typename... args>
+struct is_constructible {
+private:
+    template <typename T_, typename... args_>
+    static int f (...) {
+        return 0;
+    }
+    
+    template <typename T_, typename... args_>
+    static decltype (T_ (args_()...), char ()) f (int x) {
+        return 0;
+    }
+public:
+    static const bool value = std::is_same<decltype (f<T, args...> (0)), char>::value;
+};
+
+int main () {
+    std::cout << is_constructible<S, int>::value << is_constructible<S, int, int>::value;
+}
